8d2e55fb45a5d950b721714b5ac95c9eeafdbbc9
boisseau <sboisseau@aepsilon.com>
2022-05-12 09:10:37 +0200

    first commit

---
+++ sampleSpringData/src/main/java/com/aepsilon/sampleSpringData/SampleSpringDataApplication.java
@@ -0,0 +1,13 @@
+package com.aepsilon.sampleSpringData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringDataApplication.class, args);
+	}
+
+}
---
+++ sampleSpringData/src/test/java/com/aepsilon/sampleSpringData/SampleSpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aepsilon.sampleSpringData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
